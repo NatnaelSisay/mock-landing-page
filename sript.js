1691d75ab08336906afe1375e02152ce0a45de70
NatnaelSisay <natyjava8@gmail.com>
2021-06-17 15:35:24 +0300

    add background image

--- sript.js
+++ sript.js
@@ -15,5 +15,30 @@
 
   window.addEventListener( 'load', () => {
     start()
+
+    // background images
+    const backgroundImages = [
+      './background-2.jpeg',
+      './background-3.jpeg',
+      './background-4.jpeg',
+      './background-5.jpeg',
+      './card-background-1.jpeg'
+    ]
+
+    const body = document.querySelector( 'body' )
+
+    let imageIndex = 0
+    setInterval( () => {
+      console.log( 'called' )
+      if ( imageIndex >= backgroundImages.length ) {
+        imageIndex = 0
+        body.style.backgroundImage = `url(${ backgroundImages[imageIndex] })`
+      }
+      body.style.backgroundImage = `url(${ backgroundImages[imageIndex] })`
+      imageIndex += 1
+    }, 6000 )
+
+
   } )
+
 } )()
